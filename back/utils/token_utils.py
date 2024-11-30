b0e917554dd33b3ee13e0913032954bd743fe3a5
Bao <hgbao226@gmail.com>
2024-11-30 16:58:04 +0700

    fix token logic

--- back/utils/token_utils.py
+++ back/utils/token_utils.py
@@ -1,33 +1,53 @@
+import datetime
 import jwt
 import os
 from functools import wraps
-from flask import request, jsonify
-from config import Config
+from flask import make_response, request, jsonify
 
 JWT_SECRET = os.getenv("JWT_SECRET")
 
 def create_jwt_token(user):
+    if user["role"] == "admin":
+        expiration_time = datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(hours=1)
+    else:
+        expiration_time = datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(days=3)
+
     payload = {
         "user_id": str(user["_id"]),
         "email": user["email"],
-        "role": user["role"]
+        "role": user["role"],
+        "exp": int(expiration_time.timestamp())
     }
-    token = jwt.encode(payload, Config.SECRET_KEY, algorithm="HS256")  # Use Config.SECRET_KEY
+    token = jwt.encode(payload, JWT_SECRET, algorithm="HS256")
     return token
 
+
+def decode_and_get_role(token):
+    decoded_role = jwt.decode(token, JWT_SECRET, algorithms=["HS256"])
+    if decoded_role.get("role") == "admin":
+        return "admin"
+    else:
+        return "user"
 
 
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        token = request.headers.get('Authorization')
+        token = None
+        
+        if 'Authorization' in request.headers:
+            token = request.headers['Authorization'].split(" ")[1]
+
         if not token:
-            return jsonify({"message": "Token is missing"}), 401
+            return make_response(jsonify({"message": "A valid token is missing!"}), 401)
+        
         try:
-            decoded = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
+            decoded_payload = jwt.decode(token, JWT_SECRET, algorithms=["HS256"])
+            print(decoded_payload)
         except jwt.ExpiredSignatureError:
-            return jsonify({"message": "Token expired"}), 401
+            return make_response(jsonify({"message": "Token expired"}), 401)
         except jwt.InvalidTokenError:
-            return jsonify({"message": "Invalid token"}), 401
-        return f(decoded, *args, **kwargs)
+            return make_response(jsonify({"message": "Invalid token"}), 401)
+
+        return f(decoded_payload, *args, **kwargs)
     return decorated
