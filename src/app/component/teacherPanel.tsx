43f3f3d7deb6f95863f4386d0c9c7a8d464cb204
Bao <hgbao226@gmail.com>
2024-12-27 11:03:30 +0700

    update teacher panel

---
+++ src/app/component/teacherPanel.tsx
@@ -0,0 +1,78 @@
+"use client";
+import { Container } from "@mui/material";
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import Tabs from "@mui/material/Tabs";
+import * as React from "react";
+import InfoCard from "./infoCard";
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      className={"w-full"}
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Container>
+            <Box>
+              {children}
+            </Box>
+          </Container>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `vertical-tab-${index}`,
+    "aria-controls": `vertical-tabpanel-${index}`
+  };
+}
+
+export default function TeacherTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1, bgcolor: "background.paper", display: "flex" }}>
+      <Tabs
+        orientation="vertical"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        sx={{ borderRight: 1, borderColor: "divider" }}
+      >
+        <Tab label="Information" {...a11yProps(0)} />
+        <Tab label="Course management" {...a11yProps(1)} />
+        <Tab label="Learner management" {...a11yProps(2)} />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        <InfoCard></InfoCard>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+    </Box>
+  );
+}
