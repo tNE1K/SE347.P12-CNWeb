7bac78d74be25ec7d0aa1c58457c4d172cbe9299
vudinhan2525 <vudinhan000@gmail.com>
2024-12-27 15:19:41 +0700

    fix api

--- back/config.py
+++ back/config.py
@@ -12,4 +12,5 @@ class Config:
     MONGO_URI = os.getenv("MONGO_URI")
     JWT_SECRET = os.getenv("JWT_SECRET")
     CONFIRM_SECRET = os.getenv("CONFIRM_SECRET")
+    AZURE_STORAGE_CONNECTION_STRING = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
     TEACHER_VERIFY_IMG_DIR = 'img/teacher/verify'
